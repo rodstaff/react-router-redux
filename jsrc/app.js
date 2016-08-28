cd5bce40a5419734fd8bda8281f51c784ec96cf7
rodk <rod2010rod2010@hotmail.com>
2016-08-28 15:34:51 -0400

    completed up to step #9

--- jsrc/app.js
+++ jsrc/app.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
-import { Router, Route, Link, IndexLink, IndexRoute, hashHistory, browserHistory, DefaultRoute } from 'react-router'
+import React from 'react'
+import {Router, Route, Link, IndexLink, IndexRoute, hashHistory, browserHistory, DefaultRoute} from 'react-router'
 
-export default class App extends Component {
+export default class App extends React.Component {
   render () {
     return (
       <Router history={hashHistory}>
@@ -10,6 +10,7 @@ export default class App extends Component {
           <Route path='address' component={Address}>
             <IndexRoute component={TwitterFeed} />
             <Route path='instagram' component={Instagram} />
+            <Route path='query' component={Query} />
           </Route>
           <Route path='/about(/:name)' component={About} />
           <Route path='/namedComponent' component={NamedComponents} >
@@ -21,28 +22,6 @@ export default class App extends Component {
     )
   }
 }
-const aTitle = () => (
-  <h1>testing: Title Component</h1>
-)
-
-const aSubTitle = () => (
-  <h1>testing:  SubTitle Component</h1>
-)
-
-const NamedComponents = (props) => (
-  <div>
-    {props.title}<br />
-    {props.subTitle}
-  </div>
-)
-const Nav = () => (
-  <div>
-    <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;
-    <IndexLink activeClassName='active' to='/address'>Address</IndexLink>&nbsp;
-    <IndexLink activeClassName='active' to='/about'>About</IndexLink>
-    <IndexLink activeClassName='active' to='/namedComponent'>Named Components</IndexLink>
-  </div>
-)
 // const Nav = () => (
 //   <div>
 //     <IndexLink activeStyle={{color:'#53acff'}} to='/'>Home</IndexLink>&nbsp;
@@ -57,19 +36,38 @@ const Nav = () => (
 //     <Link to='/address'>Address</Link>
 //   </div>
 // )
-const Container = (props) => <div>
-  <Nav /> {props.children}
-</div>
 
-const Home = () => <h1>Home Sweet Home!</h1>
-
-const Address = (props) => <div>
-  <br />
-  <IndexLink activeClassName='active' to='/address'>Twitter Feed</IndexLink>&nbsp;
-  <IndexLink activeClassName='active' to='/address/instagram'>Instagram Feed</IndexLink>
-  <h1>We are located at 234 5th Avenue, New York, NY, USA.</h1>
-  {props.children}
-</div>
+const Container = (props) => (
+  <div>
+    <Nav /> 
+    {props.children}
+  </div>
+)
+const Nav = () => (
+  <div>
+    <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;&nbsp;
+    <IndexLink activeClassName='active' to='/address'>Address</IndexLink>&nbsp;&nbsp;
+    <IndexLink activeClassName='active' to='/about'>About</IndexLink>&nbsp;&nbsp;
+    <IndexLink activeClassName='active' to='/namedComponent'>Named Components</IndexLink>&nbsp;&nbsp;
+    <IndexLink activeClassName='active' 
+      to={{
+        pathname: '/address/query',
+        query: {message: 'Hello from Route Query'}
+      }}>Route Query</IndexLink>
+  </div>
+)
+const Home = () => (
+  <h1>Home Sweet Home!</h1>
+)
+const Address = (props) => (
+  <div>
+    <br />
+    <IndexLink activeClassName='active' to='/address'>Twitter Feed</IndexLink>&nbsp;&nbsp;
+    <IndexLink activeClassName='active' to='/address/instagram'>Instagram Feed</IndexLink>
+    <h1>We are located at 234 5th Avenue, New York, NY, USA.</h1>
+      {props.children}
+  </div>
+)
 // const Address = (props) => <div>
 //   <br />
 //   <Link to='/address'>Twitter Feed</Link>&nbsp;
@@ -78,15 +76,36 @@ const Address = (props) => <div>
 //   {props.children}
 // </div>
 
-const Instagram = () => <h3>Instagram Feed</h3>
-const TwitterFeed = () => <h3>Twitter Feed</h3>
+const Instagram = () => (
+  <h3>Instagram Feed</h3>
+)
+const TwitterFeed = () => (
+  <h3>Twitter Feed</h3>
+)
 const About = (props) => (
   <div>
     <h1>Welcome to the About Page</h1>
     {props.params.name && <h2>Hi there, {props.params.name}!</h2>}
   </div>
 )
-const NotFound = () => <h1>404: This page is not found!</h1>
+const NamedComponents = (props) => (
+  <div>
+    {props.title}<br />
+    {props.subTitle}
+  </div>
+)
+const aTitle = () => (
+  <h1>testing: Title Component</h1>
+)
+const aSubTitle = () => (
+  <h1>testing:  SubTitle Component</h1>
+)
+const Query = (props) => (
+  <h2>{props.location.query.message}</h2>
+)
+const NotFound = () => (
+  <h1>404: This page is not found!</h1>
+)
 
 // export default App // -> this has the same effect as 'export default class App...' above!
 
