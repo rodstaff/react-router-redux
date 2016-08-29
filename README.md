2f28bba1d83961304cffd49be961052d0bec1c79
rodk <rod2010rod2010@hotmail.com>
2016-08-29 09:29:13 -0400

    refactored the constants list

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ref:  Beginner's Guide to React Router by Nader Dabit
 link: https://medium.com/@dabit3/beginner-s-guide-to-react-router-53094349669#.sk6cq96jo
 
-This tutorial is in principle very helpful to understand React Routing.  However, one has to look at the folder structure, the file naming, the .json/.config files and the code itself very carefully to see many areas for improvement.  Unless you do so, the app may not work at the start and may look a little bit strange, so look in detail.  Although not even close to being optimized, the code presented here have been modified to some extent.  Please take look and enjoy!
+This tutorial is in principle very helpful to understand React Routing.  However, one has to look at the folder structure, the file naming, the .json/.config files and the code itself very carefully to see many areas for improvement.  Unless you do so, the app may not work at the start and may look a little bit strange, so look in detail.  The code presented here have been modified and refactored to some extent.  Please take look and enjoy coding!
 
--- jsrc/app.js
+++ jsrc/app.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import {Router, Route, Link, IndexLink, IndexRoute, hashHistory, browserHistory, DefaultRoute} from 'react-router'
+import {Container, Home, Address, TwitterFeed, Instagram, Query, About, NamedComponents, aTitle, aSubTitle, NotFound} from './constants'
 
 export default class App extends React.Component {
   render () {
+
     return (
       <Router history={hashHistory}>
         <Route path='/' component={Container}>
@@ -22,90 +24,3 @@ export default class App extends React.Component {
     )
   }
 }
-// const Nav = () => (
-//   <div>
-//     <IndexLink activeStyle={{color:'#53acff'}} to='/'>Home</IndexLink>&nbsp;
-//     <IndexLink activeStyle={{color:'#53acff'}} to='/address'>Address</IndexLink>&nbsp;
-//     <IndexLink activeStyle={{color:'#53acff'}} to='/about'>About</IndexLink>
-//   </div>
-// )
-
-// const Nav = () => (
-//   <div>
-//     <Link to='/'>Home</Link>&nbsp;
-//     <Link to='/address'>Address</Link>
-//   </div>
-// )
-
-const Container = (props) => (
-  <div>
-    <Nav /> 
-    {props.children}
-  </div>
-)
-const Nav = () => (
-  <div>
-    <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;&nbsp;
-    <IndexLink activeClassName='active' to='/address'>Address</IndexLink>&nbsp;&nbsp;
-    <IndexLink activeClassName='active' to='/about'>About</IndexLink>&nbsp;&nbsp;
-    <IndexLink activeClassName='active' to='/namedComponent'>Named Components</IndexLink>&nbsp;&nbsp;
-    <IndexLink activeClassName='active' 
-      to={{
-        pathname: '/address/query',
-        query: {message: 'Hello from Route Query'}
-      }}>Route Query</IndexLink>
-  </div>
-)
-const Home = () => (
-  <h1>Home Sweet Home!</h1>
-)
-const Address = (props) => (
-  <div>
-    <br />
-    <IndexLink activeClassName='active' to='/address'>Twitter Feed</IndexLink>&nbsp;&nbsp;
-    <IndexLink activeClassName='active' to='/address/instagram'>Instagram Feed</IndexLink>
-    <h1>We are located at 234 5th Avenue, New York, NY, USA.</h1>
-      {props.children}
-  </div>
-)
-// const Address = (props) => <div>
-//   <br />
-//   <Link to='/address'>Twitter Feed</Link>&nbsp;
-//   <Link to='/address/instagram'>Instagram Feed</Link>
-//   <h1>We are located at 234 5th Avenue, New York, NY, USA.</h1>
-//   {props.children}
-// </div>
-
-const Instagram = () => (
-  <h3>Instagram Feed</h3>
-)
-const TwitterFeed = () => (
-  <h3>Twitter Feed</h3>
-)
-const About = (props) => (
-  <div>
-    <h1>Welcome to the About Page</h1>
-    {props.params.name && <h2>Hi there, {props.params.name}!</h2>}
-  </div>
-)
-const NamedComponents = (props) => (
-  <div>
-    {props.title}<br />
-    {props.subTitle}
-  </div>
-)
-const aTitle = () => (
-  <h1>testing: Title Component</h1>
-)
-const aSubTitle = () => (
-  <h1>testing:  SubTitle Component</h1>
-)
-const Query = (props) => (
-  <h2>{props.location.query.message}</h2>
-)
-const NotFound = () => (
-  <h1>404: This page is not found!</h1>
-)
-
-// export default App // -> this has the same effect as 'export default class App...' above!
-
