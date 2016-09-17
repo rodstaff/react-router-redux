3ca0e43b35f2f30331cf6390d27bb4a224788b39
rodk <rod2010rod2010@hotmail.com>
2016-09-16 22:22:11 -0400

    only 'Home' 'Address' and 'About' links left

--- jsrc/constants.js
+++ jsrc/constants.js
@@ -22,25 +22,10 @@ export const Home = () => (
 export const Address = (props) => (
   <div>
     <br />
-    <IndexLink activeClassName='active' to='/address'>Twitter Feed</IndexLink>&nbsp;&nbsp;
-    <IndexLink activeClassName='active' to='/address/instagram'>Instagram Feed</IndexLink>
+    <h1>Hello Hello!</h1>
     <h1>We are located at 234 5th Avenue, New York, NY, USA.</h1>
       {props.children}
   </div>
-)
-// const Address = (props) => <div>
-//   <br />
-//   <Link to='/address'>Twitter Feed</Link>&nbsp;
-//   <Link to='/address/instagram'>Instagram Feed</Link>
-//   <h1>We are located at 234 5th Avenue, New York, NY, USA.</h1>
-//   {props.children}
-// </div>
-
-export const Instagram = () => (
-  <h3>Instagram Feed</h3>
-)
-export const TwitterFeed = () => (
-  <h3>Twitter Feed</h3>
 )
 export const About = (props) => (
   <div>
@@ -48,39 +33,3 @@ export const About = (props) => (
     {props.params.name && <h2>Hi there, {props.params.name}!</h2>}
   </div>
 )
-export const NamedComponents = (props) => (
-  <div>
-    {props.title}<br />
-    {props.subTitle}
-  </div>
-)
-export const aTitle = () => (
-  <h1>testing: Title Component</h1>
-)
-export const aSubTitle = () => (
-  <h1>testing:  SubTitle Component</h1>
-)
-export const Query = (props) => (
-  <h2>{props.location.query.message}</h2>
-)
-export const NotFound = () => (
-  <h1>404: This page is not found!</h1>
-)
-
-// export default App // -> this has the same effect as 'export default class App...' above!
-//    return (
-// const Nav = () => (
-//   <div>
-//     <IndexLink activeStyle={{color:'#53acff'}} to='/'>Home</IndexLink>&nbsp;
-//     <IndexLink activeStyle={{color:'#53acff'}} to='/address'>Address</IndexLink>&nbsp;
-//     <IndexLink activeStyle={{color:'#53acff'}} to='/about'>About</IndexLink>
-//   </div>
-// )
-
-// const Nav = () => (
-//   <div>
-//     <Link to='/'>Home</Link>&nbsp;
-//     <Link to='/address'>Address</Link>
-//   </div>
-// )
-//<div>
